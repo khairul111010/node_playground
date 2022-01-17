7de504fd4210a21f38c9862be1a08d0af11ef700
khairul111010 <khairul18103199@gmail.com>
2022-01-18 00:50:14 +0600

    using Body parser for key value pair data

--- node_playground/app.js
+++ node_playground/app.js
@@ -1,16 +1,22 @@
 // const http = require("http");
 
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
-app.use("/", (req, res, next) => {
-  res.send("First route");
-  //   next(); //searches for the next middleware
-});
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/path", (req, res, next) => {
-  console.log("in the next middleware");
-  res.send("<h1>HELLO WORLD</h1>"); // send is an method that makes req and response easier by doing the hardcoding automatically
+  //   console.log("in the next middleware");
+  res.send(
+    '<form action="/" method="POST"><input type="text" name="title"><button>SEND</button></input></form>'
+  ); // send is an method that makes req and response easier by doing the hardcoding automatically
+});
+
+app.use("/", (req, res, next) => {
+  console.log(req.body);
+  res.send("First route");
+  //   next(); //searches for the next middleware
 });
 
 // const server = http.createServer(app); // creating the server
