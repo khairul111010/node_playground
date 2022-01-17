c847f97ac949a87bb026a491d673f7623e6bff26
khairul111010 <khairul18103199@gmail.com>
2022-01-18 00:12:26 +0600

    node express using send

--- node_playground/app.js
+++ node_playground/app.js
@@ -11,6 +11,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log("in the next middleware");
+  res.send("<h1>HELLO WORLD</h1>"); // send is an method that makes req and response easier by doing the hardcoding automatically
 });
 
 const server = http.createServer(app); // creating the server
