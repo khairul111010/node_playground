ba160204933acee2f30497ccf0e17b33b9b01d81
khairul111010 <khairul18103199@gmail.com>
2022-01-18 00:32:20 +0600

    routing order matters. NEXT should not be used as it calles next middleware while routing into specific routes

--- node_playground/app.js
+++ node_playground/app.js
@@ -3,13 +3,12 @@
 const express = require("express");
 
 const app = express();
-
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  next(); //searches for the next middleware
+app.use("/", (req, res, next) => {
+  res.send("First route");
+  //   next(); //searches for the next middleware
 });
 
-app.use((req, res, next) => {
+app.use("/path", (req, res, next) => {
   console.log("in the next middleware");
   res.send("<h1>HELLO WORLD</h1>"); // send is an method that makes req and response easier by doing the hardcoding automatically
 });
