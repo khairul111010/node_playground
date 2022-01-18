9ab364065c6fcf30f8a28734bc833053ff8d3917
Khairul Hasan <khairul18103199@gmail.com>
2022-01-18 19:58:14 +0600

    using pug and rendering the files sending variables

--- practice_project/app.js
+++ practice_project/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "views");
+
 app.get("/", (req, res, next) => {
-  res.render();
+  res.render("index", { pageTitle: "Add Users" });
 });
 
 app.get("/users", (req, res, next) => {
-  res.render();
+  res.render("users", { pageTitle: "Users" });
 });
 
 app.post("/add-user", (req, res, next) => {
