0c458c5a84b77842e428acdc7b9aa9faf5684327
Khairul Hasan <khairul18103199@gmail.com>
2022-01-18 20:09:59 +0600

    sending request from the app.js routes making logics and using the body parser

--- practice_project/app.js
+++ practice_project/app.js
@@ -1,19 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+const users = [];
+
 app.set("view engine", "pug");
 app.set("views", "views");
+
+// app.use(bodyParser({ extended: false }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 app.get("/", (req, res, next) => {
   res.render("index", { pageTitle: "Add Users" });
 });
 
 app.get("/users", (req, res, next) => {
-  res.render("users", { pageTitle: "Users" });
+  res.render("users", { pageTitle: "Users", users: users });
 });
 
 app.post("/add-user", (req, res, next) => {
+  users.push({ name: req.body.username });
   res.redirect("/users");
 });
 
