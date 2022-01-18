f6cf34181a6f23e0f20dd4e81cb95e1b584e00bc
Khairul Hasan <khairul18103199@gmail.com>
2022-01-18 19:32:10 +0600

    basic route creation and templating engines like handlebars and pug

--- practice_project/app.js
+++ practice_project/app.js
@@ -2,4 +2,16 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res, next) => {
+  res.render();
+});
+
+app.get("/users", (req, res, next) => {
+  res.render();
+});
+
+app.post("/add-user", (req, res, next) => {
+  res.redirect("/users");
+});
+
 app.listen(3000);
